ed851f414f8755310d4cbadefb5c60d25b1a61fe
Thomas <mcdonnellthomas89@gmail.com>
2018-09-15 14:50:24 +0100

    Email and File Error logging

--- config.py
+++ config.py
@@ -5,10 +5,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-
+    # Database config
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'sqlite:///' + os.path.join(basedir, 'app.db')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    RECORDS_PER_PAGE = 10 
-
+    # Pagination config
+    RECORDS_PER_PAGE = 10
+    # Email Server config
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['mcdonnellthomas89@gmail.com']
+   
